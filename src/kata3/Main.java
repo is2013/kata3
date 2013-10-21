6d58390f0426330d2d3a88be7a57ae2939e5f4b7
JEvora <jose.evora@siani.es>
2013-10-21 23:55:51 +0200

    First commit

---
+++ src/kata3/Main.java
@@ -0,0 +1,9 @@
+package kata3;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CalculateMailDomainHistogramControl control = new CalculateMailDomainHistogramControl(args[0]);
+        control.execute();
+    }
+}
